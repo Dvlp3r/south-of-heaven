b5a6c4d42358ca1e09bdf6a1c17ee41dff773ee5
Steven Worley <bitcopia@gmail.com>
2015-10-03 20:17:09 -0400

    create waiver controller and form partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :artists
+
+  # get '/waiver' => 'waiver#show'
+  # post '/waiver-submit' => 'waiver#submit'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
