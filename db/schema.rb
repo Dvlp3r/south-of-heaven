a3ea7c8d127d55a4dbfb54a4781bbf82678beb0b
Steven Worley <bitcopia@gmail.com>
2015-10-28 21:05:02 -0400

    add artist bio and profile pic

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151015222644) do
+ActiveRecord::Schema.define(version: 20151028233335) do
 
   create_table "artists", force: :cascade do |t|
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "name"
     t.string   "instagramtag"
+    t.text     "bio"
+    t.string   "pic"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,5 +39,11 @@ ActiveRecord::Schema.define(version: 20151015222644) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "waivers", force: :cascade do |t|
+    t.string   "filename"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
