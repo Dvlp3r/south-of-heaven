49923e95692bcbb058c7471f85e0b95af5373ac0
Volney Tejeda <volneytejeda10@gmail.com>
2016-01-15 13:08:51 -0500

    improve flash success for waiver

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
-def waiver
-  @piercing = Piercing.new
-  @tattoo = Tattoo.new
-end
+  def waiver
+    @piercing = Piercing.new
+    @tattoo = Tattoo.new
+  end
+
+  def index
+    flash.keep(:success)
+  end
 end
