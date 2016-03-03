8eba2b4f8b2494ce50580f00de86eb6f54fda840
Volney Tejeda <volneytejeda10@gmail.com>
2016-03-03 15:10:58 -0500

    add user routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
 
   devise_for :users
+  devise_scope :user do
+    get 'sign_in', :to => 'devise/sessions#new', as: :sign_in
+    get 'sign_up', :to => 'devise/registrations#new', as: :sign_up
+    get 'sign_out', :to => 'devise/sessions#destroy', as: :sign_out
+  end
   resources :artists
 
   root 'pages#index'
@@ -20,7 +25,7 @@ Rails.application.routes.draw do
 
   post 'tattoo', to: 'tattoos#create', as: 'tattoo_waiver'
   get 'tattoo', to: 'pages#waiver'
-  
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
